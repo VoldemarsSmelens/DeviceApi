9cbab081f5d92524aaaa1ff8119fd848a28cfb9c
Voldemārs Smelēns <voldemars.smelens@gmail.com>
2021-11-07 21:28:28 +0200

    DeviceTopology class clean up from swagger annotations

--- src/main/java/lv/id/voldemars/deviceapi/models/DeviceTopology.java
+++ src/main/java/lv/id/voldemars/deviceapi/models/DeviceTopology.java
@@ -11,19 +11,14 @@ import java.util.List;
 
 @Data
 @Accessors(chain = true)
-@Schema(description = "Network device")
 public class DeviceTopology implements DeviceInterface,  Serializable {
 
-    @Schema(description = "Network device type", example = "SWITCH")
     private DeviceType deviceType;
 
-    @Schema(description = "Device mac address", example = "90-AB-34-9E-01-B2")
     private String macAddress;
 
-    @Schema(description = "List of connected devices")
     private List<DeviceTopology> connectedDevices = new ArrayList<>();
 
-    @Schema(description = "Up link device mac address", example = "90-AB-34-9E-01-B2")
     private transient String upLinkDeviceAddress;
 
     public void setUpLinkDevice(DeviceTopology upLinkDevice) {
